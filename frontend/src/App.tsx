a8745f137121917e188812016470eb4289fea4a5
YacineMK <merroukiyacine06@gmail.com>
2024-06-04 23:59:21 +0100

    front

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,26 @@
 
 
 export default function App() {
+  const address: string = "0x1220309339040405"
   return (
-    <h1 className="text-3xl font-bold underline">
-      hi
-    </h1>
+    <div className="w-[100vw] h-[100vh] flex flex-col justify-center items-center">
+      <div>
+        <h1>SOY TOKEN </h1>
+        <p>{address}</p>
+      </div>
+      <div>
+        <h1>Transfer</h1>
+        <form>
+          <div>
+            <p>Amount of SOY</p>
+            <input type="text" className="border" />
+          </div>
+          <div>
+            <p>Amount of SOY</p>
+            <input type="text" />
+          </div>
+        </form>
+      </div>
+    </div>
   )
 }
